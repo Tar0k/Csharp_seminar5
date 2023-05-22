fffea6697e2af76dd3fccd692a4d6e29aa370ad5
Tar0k <73.tarok@gmail.com>
2023-05-22 23:32:55 +0400

    Initial commit

---
+++ Task_36/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//     [3, 7, 23, 12] -> 19
+//     [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+var array = new int[6].Select(val => new Random().Next(0, 10)).ToArray();
+Console.Write("[" + string.Join(",", array) + "]");
+var oddPositionValues = array.Where((value, index) => index % 2 == 1).ToArray();
+//Console.Write("[" + string.Join(",", oddPositionValues) + "]");
+Console.WriteLine($" -> {oddPositionValues.Sum()}");
---
+++ Task_38/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//     Диапазон [-10, 10]. Обратите внимание на вещественных чисел
+// Для примера возьмём диапазон побольше. [3 7 22 2 78] -> 76
+
+Console.WriteLine("Задача 38: Задайте массив вещественных чисел." +
+                  " Найдите разницу между максимальным и минимальным элементов массива. Диапазон [-10, 10]." +
+                  " Обратите внимание на вещественных чисел");
+var array = new double[5].Select(val => GenerateScaledRandom()).ToArray();
+Console.WriteLine("[" + string.Join("  ", array) + "]");
+var difference = array.Max() - array.Min();
+Console.WriteLine($"Разница: {difference}");
+
+
+
+double GenerateScaledRandom() => Math.Round(20 * new Random().NextDouble() - 10, 3);
